31824d9ab0cd69f65cb13d674121616262a39e50
Irene Mizus <irene.mizus@gmail.com>
2024-09-02 12:21:58 +0300

    Batch automation added.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import copy
 import math
+import os.path
 import sys
 
 import formats
@@ -66,36 +67,79 @@ def sd_calculation(comp_states):
     return sd, Nsd
 
 
+def out_file_name_generation(full_out_folder, out_file_name, J, mol_name):
+    out_file_name_split = out_file_name.rsplit('.', 1)
+    if len(out_file_name_split) == 2:
+        out_file_name_out = ".".join([out_file_name_split[0], mol_name, "J" + J, out_file_name_split[1]])
+    else:
+        out_file_name_out = ".".join([out_file_name_split[0], mol_name, "J" + J])
+    out_file_name_full = os.path.join(full_out_folder, out_file_name_out)
+    return out_file_name_full
+
+
 if __name__ == '__main__':
-    E_zero = float(sys.argv[3])
-    file_calc_name = sys.argv[1]
+    input_folder = "input"
+    output_folder = "output"
+    file_calc_name_prefix = "fort.14"
+
+    mol_name = sys.argv[1]
     file_exp_name = sys.argv[2]
-    out_file_exp_name = sys.argv[4]
-    out_file_comp_name = sys.argv[5]
+    out_file_exp_name = sys.argv[3]
+    out_file_comp_name = sys.argv[4]
+    E_zero = float(sys.argv[5])
     eps = float(sys.argv[6])
 
-    format_calc = formats.CalcFormat(file_calc_name, E_zero)
-    states_calc = format_calc.parse_file()
+    list_inp_files = []
+    full_inp_folder = os.path.join(os.getcwd(), input_folder, mol_name)
+    if os.path.isdir(full_inp_folder):
+        list_inp_files = os.listdir(full_inp_folder)
+    else:
+        print("Input folder doesn't exist")
+        exit(1)
 
-    J_list = [str(states_calc[0].J)]
+    full_out_folder = os.path.join(os.getcwd(), output_folder, mol_name)
+    is_out_dir_exists = os.path.isdir(full_out_folder)
+    if not is_out_dir_exists:
+        os.makedirs(full_out_folder)
 
-    # for H2-16O
-    # 0 ~ v1  1 ~ v2  2 ~ v3  3 ~ J  4 ~ Ka  5 ~ Kc  6 ~ E
-    #format_exp = formats.ExpFormatH216O(file_exp_name, J_list, 3)
+    forts = []
+    for file_name in list_inp_files:
+        if file_name.startswith(file_calc_name_prefix):
+            forts.append(os.path.join(full_inp_folder, file_name))
 
-    # for N2O
-    # 0 ~ q1  1 ~ q2  2 ~ q3  3 ~ sym  4 ~ J  5 ~ E
-    format_exp = formats.ExpFormatN2O(file_exp_name, J_list, 4)
+    for fort in forts:
+        format_calc = formats.CalcFormat(fort, E_zero)
+        states_calc = format_calc.parse_file()
 
-    states_exp = format_exp.parse_file()
-    comp_list = do_comparison(states_calc, states_exp, eps)
-    comp_list.write_to_file(out_file_comp_name)
-    states_exp.write_to_file(out_file_exp_name)
+        J_list = [str(states_calc[0].J)]
 
-    comp_states = comp_list.parse_file(out_file_comp_name)
-    sd, Nsd = sd_calculation(comp_states)
-    with open(out_file_comp_name, 'a') as f:
-        f.write(f"\nsd = {sd}, N = {Nsd}\n")
+        file_exp_name_full = os.path.join(full_inp_folder, file_exp_name)
+
+        if mol_name.upper() == "H2-16O":
+            # for H2-16O
+            # 0 ~ v1  1 ~ v2  2 ~ v3  3 ~ J  4 ~ Ka  5 ~ Kc  6 ~ E
+            format_exp = formats.ExpFormatH216O(file_exp_name_full, J_list, 3)
+        elif mol_name.upper() == "N2O":
+            # for N2O
+            # 0 ~ q1  1 ~ q2  2 ~ q3  3 ~ sym  4 ~ J  5 ~ E
+            format_exp = formats.ExpFormatN2O(file_exp_name_full, J_list, 4)
+        else:
+            print("Only H2-16O and N2O molecules are supported by now")
+            exit(1)
+
+        states_exp = format_exp.parse_file()
+        comp_list = do_comparison(states_calc, states_exp, eps)
+
+        out_file_comp_name_full = out_file_name_generation(full_out_folder, out_file_comp_name, J_list[0], mol_name)
+        out_file_exp_name_full = out_file_name_generation(full_out_folder, out_file_exp_name, J_list[0], mol_name)
+
+        comp_list.write_to_file(out_file_comp_name_full)
+        states_exp.write_to_file(out_file_exp_name_full)
+
+        comp_states = comp_list.parse_file(out_file_comp_name_full)
+        sd, Nsd = sd_calculation(comp_states)
+        with open(out_file_comp_name_full, 'a') as f:
+            f.write(f"\nsd = {sd}, N = {Nsd}\n")
 
 
 
